1e160d2adb91e90a69ba2f6fb085734643480cc5
snasir-dev <103860247+snasir-dev@users.noreply.github.com>
2025-12-19 13:45:59 -0500

    fix(script -> x-lazygit-fix-corrupted-state.sh): Change the location of the state.yml file to $LOCALAPPDATA/lazygit which is where CORRUPTED state.yml file causing issues always seems to be @ Friday, December 19, 2025 at 1:45:58 PM

--- scripts/x-lazygit-fix-corrupted-state.sh
+++ scripts/x-lazygit-fix-corrupted-state.sh
@@ -9,7 +9,10 @@
 # Define plugin destination path using LOCALAPPDATA
 # LAZYGIT_DIR="$LOCALAPPDATA/lazygit"
 # Lazygit first checks XDG_CONFIG_HOME if we set it. I have set it to '$HOME/.config' in env.sh
-LAZYGIT_DIR="$XDG_CONFIG_HOME/lazygit"
+
+# VERY IMPORTANT: 'state.yml' file ALWAYS seems to be located $HOME\AppData\Local\lazygit\state.yml IT DOES NOT ADHERE to the XDG_CONFIG_HOME standard. THIS IS THE FILE WE MUST DELETE. $LOCALAPPDATA = $HOME\AppData\Local
+LAZYGIT_DIR="$LOCALAPPDATA/lazygit"
+# LAZYGIT_DIR="$XDG_CONFIG_HOME/lazygit"
 LAZYGIT_DIR_WINDOWS_PATH=$(cygpath -w "$LAZYGIT_DIR")
 
 echo "📂 Lazygit directory (Linux path): $LAZYGIT_DIR"
